e20d8f4e419f67a1ae9d4ec5000770ee8fe82d83
Wei Sun <wei.sun@outware.com.au>
2018-03-07 14:39:31 +1100

    Add network address functions

--- system-info.1s.py
+++ system-info.1s.py
@@ -2,7 +2,6 @@
 
 import psutil as pu
 import time
-import os
 
 def humanise(byte_size=0, suffix='B'):
     size = byte_size
@@ -58,14 +57,26 @@ def load_per_core():
 def mem_percent():
     vmem = pu.virtual_memory()
     mem_load = ["Memory: {}% | font=courier color={}".format(vmem.percent, load_color(vmem.percent))]
-    mem_load.append("\n--{0:<10}: {1:>9} | font=courier".format("Total", humanise(vmem.total)))
-    mem_load.append("\n--{0:<10}: {1:>9} | font=courier".format("Available", humanise(vmem.available)))
-    mem_load.append("\n--{0:<10}: {1:>9} | font=courier".format("Used", humanise(vmem.used)))
-    mem_load.append("\n--{0:<10}: {1:>9} | font=courier".format("Free", humanise(vmem.free)))
-    mem_load.append("\n--{0:<10}: {1:>9} | font=courier".format("Active", humanise(vmem.active)))
-    mem_load.append("\n--{0:<10}: {1:>9} | font=courier".format("Inactive", humanise(vmem.inactive)))
-    mem_load.append("\n--{0:<10}: {1:>9} | font=courier".format("Wired", humanise(vmem.wired)))
+    menu_format = "\n--{0:<10}: {1:>9} | font=courier color=black"
+    mem_load.append(menu_format.format("Total", humanise(vmem.total)))
+    mem_load.append(menu_format.format("Available", humanise(vmem.available)))
+    mem_load.append(menu_format.format("Used", humanise(vmem.used)))
+    mem_load.append(menu_format.format("Free", humanise(vmem.free)))
+    mem_load.append(menu_format.format("Active", humanise(vmem.active)))
+    mem_load.append(menu_format.format("Inactive", humanise(vmem.inactive)))
+    mem_load.append(menu_format.format("Wired", humanise(vmem.wired)))
     return ''.join(mem_load)
+
+def network_info():
+    net_info = pu.net_if_addrs()
+    keys = sorted(list(net_info.keys()))
+    result = ["Network"]
+    menu_format = "\n--{}: {} | font=courier color=black"
+    for key in keys:
+        nic_address_info = (nic for nic in net_info[key] if nic.family.name == 'AF_INET')
+        for n in nic_address_info:
+            result.append(menu_format.format(key, n.address))
+    return ''.join(result)
 
 cpu, mem, bandwidth = system_info()
 cpu = "CPU: {}%".format(cpu)
@@ -75,5 +86,5 @@ print("{0:^} ❙ {1:^} ❙ {2:^} | trim=false font=courier".format(cpu, mem, ban
 print("---")
 print(load_per_core())
 print(mem_percent())
-print("Network")
+print(network_info())
 print("Refresh | refresh=true")
